e08cb85d680eb33e29a8045b8f57c09b933cb39f
fuzailkhon Abdurakhimov <fuzailkhon946@gmail.com>
2023-05-08 22:58:00 +0500

    init: files copy works !!!

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const path = require('path')
+const fs = require('fs')
 
+const source = path.join(__dirname, 'files') 
+const dest = path.join(__dirname, 'files-copy')
+
+fs.mkdir(dest, err => {
+    if (err) throw err;
+    console.log('Папка была создана');
+})
+
+fs.readdir(source, (err, files) => {
+    files.forEach((file, index) => {
+        const filePath = path.join(source, file)
+        const destPath = path.join(dest, file)
+        fs.copyFile(filePath, destPath, (err) => {
+            if (err) throw err
+        })
+    })
+})
